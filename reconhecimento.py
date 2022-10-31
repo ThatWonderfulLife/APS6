6807e4990955f14e511d88c18789b59c213d686f
ThatWonderfulLife <pedroarena4@gmail.com>
2022-10-31 18:56:06 -0300

    Concertei erros de variaveis e paths

--- reconhecimento.py
+++ reconhecimento.py
@@ -6,7 +6,7 @@ import cv2 as cv # O famigerado
 
 from google.cloud import vision_v1p3beta1 as vision
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'chave_cliente.json' #Chave para usar o serviços do Google Vision.
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = os.path.dirname(__file__) + '\\chave.json' #Chave para usar o serviços do Google Vision.
 
 # pega o path atual do arquivo 'reconhecimento.py'
 path_incial = os.path.dirname(__file__)
@@ -16,26 +16,30 @@ tipo_comida = 'Fruta'
 
 #Carrega os nomes das frutas que estão no arquivo Fruta.dict e retorna uma lista com os nomes.
 def carregar_nome_fruta (tipo_comida):
-    nomes = [line.rstrip('\n').lower() for line in open(f"{path_incial}/{tipo_comida}.dict")]
+    nomes = [line.rstrip('\n').lower() for line in open(f"{path_incial}\\{tipo_comida}.dict")]
     return nomes
 
 def reconhecer(path_incial, lista_frutas):
-    t_inicial = datetime.now()
-    img = cv.imread(f'{path_incial}/Fruta.jpg')  #Lê img do path
+    print("Passou 1")
+    img = cv.imread(f'{path_incial}\\Fruta.jpg')  #Lê img do path
+    print("Passou 2")
     height,width = img.shape[:2] #define altura e largura da imagem
+    print("Passou 3")
     img = cv.resize(img, (800, int( (height * 800) / width) ) ) #deixa a imagem menor
-    cv.imwrite(path_incial + "cortada.jpg", img) #salva a imagem temporaria no path dado
-    
+    print("Passou 4")
+    cv.imwrite(path_incial + "\\cortada.jpg", img) #salva a imagem temporaria no path dado
+    print("Passou 5")
     path_foto_cortada = (f"{path_incial}\\cortada.jpg") #definindo onde q fica a foto cortada
+    print("Passou 6")
     cliente = vision.ImageAnnotatorClient()  #Puxa cliente do google Vision
-
+    print("Passou 7")
     
     with io.open(path_foto_cortada, 'rb') as arquivo_imagem: #Lê imagem
         conteudo = arquivo_imagem.read()
     
-    imagem = vision.types.Image(conteudo = conteudo) #define tipo da imagem como seu proprio conteudo para o Visions (nao entendo como o vision funciona só aceito)
+    imagem = vision.types.Image(content = conteudo) #define tipo da imagem como seu proprio conteudo para o Visions (nao entendo como o vision funciona só aceito)
    
-    resposta = cliente.label_detection(imagem = imagem)  #Puxa todos os resultados que o Visions deu e salva numa variavel
+    resposta = cliente.label_detection(image = imagem)  #Puxa todos os resultados que o Visions deu e salva numa variavel
     labels = resposta.anotacoes_label
     
     for label in labels: #A variavel é uma lista, esse pedaço itera todos os itens da lista e arredonda os valores que vieram de brinde também, depois divide eles em 'label' e 'Pontos' pra cada resultado
@@ -53,6 +57,6 @@ def reconhecer(path_incial, lista_frutas):
 
 lista_treco = carregar_nome_fruta(tipo_comida) #inicia o algoritimo
 print(lista_treco) #mostra o resultado
-path = (f'{path_incial}laranja.jpg') #Salva ele
+path = path_incial #Salva ele
 
 reconhecer(path,lista_treco) #reconhece fruta
--- testes.py
+++ testes.py
@@ -1,5 +1,19 @@
-path_foto = "C:/Users/ThatWonderfulLife/Desktop/APS6/"
+import cv2 as cv
+import os
+path_inicial = os.path.dirname(__file__)
+path_foto = os.path.dirname(__file__)
 
 path_foto_cortada = (f"{path_foto}cortada.jpg")
 
 print(path_foto_cortada)
+
+img = cv.imread(f'{path_inicial}\\Fruta.jpg')
+
+height,width = img.shape[:2]
+
+
+print(height , width)
+img2 = cv.resize(img, (800, int( (height * 800) / width) ) )
+print(img2)
+
+cv.imwrite(path_inicial + "\\cortada.jpg", img2)
