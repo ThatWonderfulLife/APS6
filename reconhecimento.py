3c928563f53db3ce98c7acd7dccc7a599dc3d0ad
ThatWonderfulLife <pedroarena4@gmail.com>
2022-11-04 10:39:04 -0300

    Atualização do codigo e teste de reconhecimento

--- reconhecimento.py
+++ reconhecimento.py
@@ -20,19 +20,18 @@ def carregar_nome_fruta (tipo_comida):
     return nomes
 
 def reconhecer(path_incial, lista_frutas):
-    print("Passou 1")
+    
     img = cv.imread(f'{path_incial}\\Fruta.jpg')  #Lê img do path
-    print("Passou 2")
+    
     height,width = img.shape[:2] #define altura e largura da imagem
-    print("Passou 3")
+    
     img = cv.resize(img, (800, int( (height * 800) / width) ) ) #deixa a imagem menor
-    print("Passou 4")
+    
     cv.imwrite(path_incial + "\\cortada.jpg", img) #salva a imagem temporaria no path dado
-    print("Passou 5")
+    
     path_foto_cortada = (f"{path_incial}\\cortada.jpg") #definindo onde q fica a foto cortada
-    print("Passou 6")
+    
     cliente = vision.ImageAnnotatorClient()  #Puxa cliente do google Vision
-    print("Passou 7")
     
     with io.open(path_foto_cortada, 'rb') as arquivo_imagem: #Lê imagem
         conteudo = arquivo_imagem.read()
@@ -40,7 +39,7 @@ def reconhecer(path_incial, lista_frutas):
     imagem = vision.types.Image(content = conteudo) #define tipo da imagem como seu proprio conteudo para o Visions (nao entendo como o vision funciona só aceito)
    
     resposta = cliente.label_detection(image = imagem)  #Puxa todos os resultados que o Visions deu e salva numa variavel
-    labels = resposta.anotacoes_label
+    labels = resposta.label_annotations
     
     for label in labels: #A variavel é uma lista, esse pedaço itera todos os itens da lista e arredonda os valores que vieram de brinde também, depois divide eles em 'label' e 'Pontos' pra cada resultado
         descricao = label.description.lower()
